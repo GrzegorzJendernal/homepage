99a7dac9ef1054893f7e68f5c76efbfae53fc319
Grzegorz Jendernal <g.jendernal@gmail.com>
2022-08-18 16:01:58 +0200

    added arrow functions

--- js/script.js
+++ js/script.js
@@ -1,10 +1,19 @@
-console.log("Hej!");
-let brokePage = document.querySelector(".js-brokePage");
-let uglyStyle = document.querySelector(".body__broken");
-let body = document.querySelector(".body");
-let buttonText = document.querySelector(".js-buttonText");
+{
+    const helloWorld = () => {
+        console.log("Hej! Życzę miłego przeglądania kodu :)");
+    };
 
-brokePage.addEventListener("click", () => {
-    body.classList.toggle("body__broken");
-    buttonText.innerText = body.classList.contains("body__broken") ? "Napraw" : "Zepsuj";
-});
+    const onBrokePageClick = () => {
+        const body = document.querySelector(".body");
+        const buttonText = document.querySelector(".js-buttonText");
+        body.classList.toggle("body__broken");
+        buttonText.innerText = body.classList.contains("body__broken") ? "Napraw" : "Zepsuj";
+    }
+
+    const init = () => {
+        const brokePage = document.querySelector(".js-brokePage");
+        brokePage.addEventListener("click", onBrokePageClick);
+        helloWorld();
+    }
+    init();
+}
